8b9527e23fe4e8f6ac22a9cf47e030ca3896ffde
Lamhot Simamora <pdrtechnology93@gmail.com>
2024-09-13 18:00:18 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var $ = Dom7;
+
+
+var app = new Framework7({
+  name: 'helloWorld', // App name
+  theme: 'auto', // Automatic theme detection
+  colors: {
+    primary: '#7d5fff',
+  },
+
+  el: '#app', // App root element
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+  // Register service worker
+  serviceWorker: {
+    path: '/service-worker.js',
+  },
+});
+// Login Screen Demo
+$('#my-login-screen .login-button').on('click', function () {
+  var username = $('#my-login-screen [name="username"]').val();
+  var password = $('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
+});
